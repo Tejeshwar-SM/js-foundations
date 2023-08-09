b3363de6a62e81ab3cb69efe707045db354af9ce
Tejeshwar-SM <101811200+Tejeshwar-SM@users.noreply.github.com>
2023-08-09 14:38:38 +0000

    04-part1-complete

---
+++ 01-basics/03-strings.js
@@ -0,0 +1,29 @@
+const myName = "Tejeshwar";
+const repoCount = 50;
+// console.log(myName+repoCount+" Hello world");
+
+// console.log(`Hello my name is ${myName} and my repo count is ${repoCount}`);
+const gameName = new String("Lessaagoo");
+// console.log(gameName);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf('ago'));
+
+const anotherString = gameName.substring(0,3);
+// console.log(anotherString);
+// const newString = gameName.slice(-5,7);
+// console.log(newString);
+
+const newStringOne = "  Tejeshwar Singh";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https:/dadjksada.com/fasd adada20%danld";
+// console.log(url.replace('20%', "_-_"));
+// console.log(url.replace(' ', "_-_"));
+// console.log(url.includes('dadjk'));
+
+console.log(url.split('j'));
+
+
+
---
+++ 01-basics/04-numbersAndMaths.js
@@ -0,0 +1,13 @@
+const balance = new Number(1000);
+// console.log(balance);
+// balance.toString();
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const anotherNum = 282193.3279331;
+// console.log(anotherNum.toPrecision(5));
+//will be problematic, so use carefully. it gives 2.8219e+5
+// const hundreds = 1000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++=
