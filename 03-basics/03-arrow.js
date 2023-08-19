dd90e8723d7738facbcc865cb9796a2660f905c2
Tejeshwar-SM <101811200+Tejeshwar-SM@users.noreply.github.com>
2023-08-19 06:45:10 +0000

    19Aug-Task1.1 Complete

--- 03-basics/03-arrow.js
+++ 03-basics/03-arrow.js
@@ -1,6 +1,39 @@
-const obj = {
-    username: "tejeshwar",
-    price: "priceless"
+// const obj = {
+//     username: "tejeshwar",
+//     price: "399!",
+//     welcomeMessage: function () {
+//         console.log(`Hey there ${this.username}!, Welcome to our pod!!`);
+//         // console.log(this); --> returns this object
+//     }
+// }
+// obj.welcomeMessage();
+// obj.username = "Sic Mundus"
+// obj.welcomeMessage();
+// console.log(this) -->> Returns an empty object
 
-}
-console.log(this.username);
+
+// function one() {
+//     let username = "Raju"
+//     console.log(this.username)
+// }
+// one();
+// const one = function () {
+//     let username = "Raju"
+//     console.log(this.username)
+// }
+// one();
+// const one = () => {
+//     let username = "Bheem"
+//     console.log(this.username);
+// }
+// one();
+
+const addTwo = (num1,num2) =>  (num1+num2)
+const addTwo2 = (num1,num2) => {return num1+num2;}
+const addTwo3 = (num1,num2) =>  ({username: "Tejeshwar"})
+
+//----> to return objects, wrap them in ()
+
+console.log(addTwo(1,2));
+console.log(addTwo2(3,4));
+console.log(addTwo3(3,4));
